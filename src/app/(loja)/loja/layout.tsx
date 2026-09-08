838aad765c407234bfc8daa331d2f06717ce7dee
felipemenezes25000-spec <felipemenezes25000@gmail.com>
2026-09-07 21:01:32 -0300

    Aplica tema premium ao catálogo de equipamentos

---
+++ src/app/(loja)/loja/layout.tsx
@@ -0,0 +1,7 @@
+export default function CatalogoEquipamentosLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="catalogo-premium catalogo-equipamentos">{children}</div>;
+}
